cfc9327f4064704dabfac7cacd3db70ab373a3c6
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-06-25 17:37:17 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/Exception.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $to = "brynley.bp@gmail.com"; // Your Email address
     $from = $_POST['email']; // Sender's Email address
@@ -6,24 +12,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $subject = "Thank you for contacting Galactic AIO";
     $message = "Dear $name,\n\nThank you for reaching out to us. We have received your message and will get back to you shortly.\n\nBest regards,\nGalactic AIO Team";
 
-    $headers = "From: Galactic AIO <noreply@galacticaio.com>\r\n";
-    $headers .= "Reply-To: $to\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $mail = new PHPMailer(true);
 
-    mail($from, $subject, $message, $headers);
+    try {
+        // Server settings
+        $mail->isMail();
 
-    $subject2 = "New Contact Form Submission";
-    $message2 = "Name: $name\nEmail: $from\n\nMessage:\n" . $_POST['message'];
+        // Recipients
+        $mail->setFrom('noreply@galacticaio.com', 'Galactic AIO');
+        $mail->addAddress($to);
 
-    $headers2 = "From: $from\r\n";
-    $headers2 .= "Reply-To: $from\r\n";
-    $headers2 .= "MIME-Version: 1.0\r\n";
-    $headers2 .= "Content-Type: text/plain; charset=UTF-8\r\n";
+        // Content
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->CharSet = 'UTF-8';
 
-    mail($to, $subject2, $message2, $headers2);
+        $mail->send();
 
-    echo json_encode(['status' => 'success', 'message' => 'Thank you for contacting us! We will be in touch shortly.']);
-    exit;
+        echo json_encode(['status' => 'success', 'message' => 'Thank you for contacting us! We will be in touch shortly.']);
+        exit;
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => 'An error occurred while sending the message.']);
+        exit;
+    }
 }
 ?>
