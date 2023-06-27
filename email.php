bbc934d761cefce3e4d4f59704168a55c838ff6e
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-06-27 19:05:30 +0100

    Update email.php

--- email.php
+++ email.php
@@ -4,7 +4,7 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/SMTP.php'; // Add this line
+require 'PHPMailer/src/SMTP.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $to = "brynley.bp@gmail.com"; // Your Email address
