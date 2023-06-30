1e3bfc0024ec1d4b38174fa87b519a07b0736207
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-06-30 21:39:48 +0100

    Update email.php

--- email.php
+++ email.php
@@ -6,33 +6,33 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-if (isset($_POST["send"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
     $mail = new PHPMailer(true);
-    
-    $mail->isSMTP(); // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = 'brynley.bp@gmail.com'; // Your Gmail address
-    $mail->Password = 'zvkovilysqsezypi'; // Your Gmail app password
-    $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465; // TCP port to connect to
-    
-    // Sender information
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'brynley.bp@gmail.com';
+    $mail->Password = 'zvkovilysqsezypi';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
     $mail->setFrom('brynley.bp@gmail.com');
-    $mail->addAddress($_POST["email"]);
+    $mail->addAddress($email);
 
     $mail->isHTML(true);
 
-    $mail->Subject = $_POST["subject"];
-    $mail->Body = $_POST["message"];
+    $mail->Subject = "Message from Contact Form";
+    $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
 
-    $mail->send();
-
-    echo "
-    <script>
-    alert('sent');
-    document.location.href = 'index.html';
-    </script>
-    ";
+    if ($mail->send()) {
+        echo "<script>alert('Email sent'); window.location.href = 'index.html';</script>";
+    } else {
+        echo "<script>alert('Email could not be sent'); window.location.href = 'index.html';</script>";
+    }
 }
 ?>
