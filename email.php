c1dfa4c2c64dba56392a998642f5bca7fa7f9d97
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-06-30 20:55:48 +0100

    Update email.php

--- email.php
+++ email.php
@@ -2,6 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require 'PHPMailer/autoload.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/SMTP.php';
