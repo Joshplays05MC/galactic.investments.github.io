e4a99dd27f527cfe396b00010b4c904714187ca2
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-06-30 21:16:41 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,62 +1,42 @@
 <?php
+// Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/autoload.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/SMTP.php';
+require_once 'vendor/autoload.php';
 
-// Load SMTP configuration from a separate file
-require 'config.php';
+$mail = new PHPMailer(true);
+$mail->isSMTP(); // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+$mail->SMTPAuth = true; // Enable SMTP authentication
+$mail->Username = 'brynley.bp@gmail.com'; // Your Gmail address
+$mail->Password = 'zvkovilysqsezypi'; // Your Gmail app password
+$mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to
+// Sender information
+$mail->setFrom('noreply@galactic.investments.com', 'Galactic Investments');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Validate form input
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+// Receiver address and name
+$mail->addAddress('R', 'RECEPIENT_NAME');
 
-    if (!$email || !$name || !$message) {
-        echo json_encode(['status' => 'error', 'message' => 'Invalid form data. Please check your inputs.']);
-        exit;
-    }
+// Add CC or BCC
+// $mail->addCC('email@mail.com');
+// $mail->addBCC('user@mail.com');
 
-    $to = "brynley.bp@gmail.com"; // Your Email address
-    $subject = "Thank you for contacting Galactic Investments";
 
-    $mail = new PHPMailer(true);
+$mail->isHTML(true);
 
-    try {
-        // Server settings
-        $mail->isSMTP(); // Set mailer to use SMTP
-        $mail->Host = $smtpHost; // Read from config file
-        $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = $smtpUsername; // Read from config file
-        $mail->Password = $smtpPassword; // Read from config file
-        $mail->SMTPSecure = $smtpSecure; // Read from config file
-        $mail->Port = $smtpPort; // Read from config file
+$mail->Subject = 'PHPMailer SMTP test';
+$mail->Body    = "<h4>PHPMailer the awesome Package</h4>
+<b>PHPMailer is working fine for sending mail</b>
+<p>This is a tutorial to guide you on PHPMailer integration</p>";
 
-        // Recipients
-        $mail->setFrom($smtpUsername, 'Galactic Investments');
-        $mail->addAddress($to);
-        $mail->addReplyTo($email, $name);
-
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body = "Dear $name,<br><br>Thank you for reaching out to us. We have received your message and will get back to you shortly.<br><br>Best regards,<br>Galactic Investments Team";
-        $mail->CharSet = 'UTF-8';
-
-        $mail->send();
-
-        echo json_encode(['status' => 'success', 'message' => 'Thank you for contacting us! We will be in touch shortly.']);
-        exit;
-    } catch (Exception $e) {
-        // Log the error
-        error_log('Email sending failed: ' . $mail->ErrorInfo);
-
-        echo json_encode(['status' => 'error', 'message' => 'An error occurred while sending the message. Please try again later.']);
-        exit;
-    }
+// Send mail
+if (!$mail->send()) {
+    echo 'Email not sent. An error was encountered: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent.';
 }
-?>
+
+$mail->smtpClose();
