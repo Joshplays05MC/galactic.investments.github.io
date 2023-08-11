d225114813f0db30b65c57d59d614d9fffbf80e5
Bryn Phillips <114364562+bryndeeny@users.noreply.github.com>
2023-08-11 14:32:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const navLinks = document.querySelectorAll('.nav-links li, .logo');
 
   burger.addEventListener('click', () => {
     // Toggle Nav
@@ -264,7 +264,6 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-
 
 
 
